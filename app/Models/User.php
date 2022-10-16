c5143fc9737d28c1c8ad1c64f77d704ce4c2abe1
Wise Dev <86822394+wisedev99@users.noreply.github.com>
2022-10-16 21:26:36 +0500

    lesson-09 When it Makes Sense to Run Additional Queries

--- app/Models/User.php
+++ app/Models/User.php
@@ -100,11 +100,15 @@ class User extends Authenticatable
             $query->where(function ($query) use ($term) {
                 $query->where('name', 'like', $term)
                     ->orWhere('email', 'like', $term)
-                    ->orwhereHas('company', function ($query) use ($term) {
+                    ->orwhereHas(
+                        'company',
+                        Company::query()
+                            ->where('name', 'like', $term)
+                            ->pluck('id')
                         // $query->where('name', 'like', $term);
-                        $query->select('id')->from('companies')
-                            ->where('name', 'like', $term);
-                    });
+                        // $query->select('id')->from('companies')
+                        //     ->where('name', 'like', $term);
+                    );
             });
         });
     }
